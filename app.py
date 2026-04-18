47ed7d1cc016d67cc31415e3315da1f2db687220
SAIVAMSI G <vamcgundlapalli@gmail.com>
2026-04-18 20:49:11 +0530

    frontend + backend connected

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Space Debris Detection Running 🚀"
+@app.route("/api")
+def api():
+    return jsonify({"message": "Backend working 🚀"})
 
 def handler(request):
     return app
