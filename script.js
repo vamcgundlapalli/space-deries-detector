0d32220c2ab5a2dcbb4f198307e1fb02a528e4ad
SAIVAMSI G <vamcgundlapalli@gmail.com>
2026-04-18 22:42:44 +0530

    show yolo results

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
-console.log("JS LOADED SUCCESSFULLY");
-
 const form = document.getElementById("uploadForm");
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    const fileInput = document.getElementById("file");
-    const file = fileInput.files[0];
-
-    if (!file) {
-        alert("Please select a file");
-        return;
-    }
+    const file = document.getElementById("file").files[0];
 
     const formData = new FormData();
     formData.append("file", file);
 
-    try {
-        const res = await fetch("https://space-backend-15tg.onrender.com/predict", {
-            method: "POST",
-            body: formData
-        });
+    const res = await fetch("https://space-backend-15tg.onrender.com/predict", {
+        method: "POST",
+        body: formData
+    });
 
-        const data = await res.json();
+    const data = await res.json();
 
-        document.getElementById("result").innerText = data.message;
-    } catch (err) {
-        console.error(err);
-        alert("Error connecting to backend");
-    }
+    let output = "Detected:<br>";
+
+    data.detections.forEach(d => {
+        output += `${d.class} (${d.confidence})<br>`;
+    });
+
+    document.getElementById("result").innerHTML = output;
 });
